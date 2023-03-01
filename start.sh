ff6acecb7d86e1e98767678460e1adfc2402e8e4
Saypanxyz <122514072+Saypanxyz@users.noreply.github.com>
2023-03-01 10:40:37 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Thepan808/Uno-BR- ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/Thepan808/Uno-BR- ok && cd find . -maxdepth 2 -type d -name 'LC_MESSAGES' -exec bash -c 'msgfmt {}/unobot.po -o {}/unobot.mo' \; && pip3 install -U -r requirements.txt
 python3 bot.py
